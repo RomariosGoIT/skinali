7f70f0c47524c1cf1fe8fc49fab4db5abc4ebbfd
Romarios <romantgt@gmail.com>
2019-03-26 22:45:35 +0200

    CSS Transforming Hamburger Menu Icon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.menu-button').addEventListener('click', () => {
+  document.querySelector('.menu-button').addEventListener('click', el => {
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.menu-button').classList.toggle('active');
   });
 });
