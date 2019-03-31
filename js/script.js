e8c10c5042c43b1830abfae27fbf5ce2870d143d
RomariosGoIT <romantgt@gmail.com>
2019-03-31 15:35:22 +0300

    menu shadow on scroll

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.menu').classList.toggle('menu_active');
     document.querySelector('.menu-button').classList.toggle('active');
   });
+
+  window.addEventListener('scroll', () => {
+    if (pageYOffset > 0) {
+      document.querySelector('.navbar').classList.add('navbar__scroll');
+    } else {
+      document.querySelector('.navbar').classList.remove('navbar__scroll');
+    }
+  });
 });
